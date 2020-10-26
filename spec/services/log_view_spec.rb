cbeb248afe3ef8ae659ed3ee149c087d107397a3
Evgenii Bugakov <evgenii_bugakov@epam.com>
2020-10-26 11:24:06 +0300

    spec view service

---
+++ spec/services/log_view_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require_relative '../../app/model/log.rb'
+
+RSpec.describe LogView do
+  describe 'service' do
+    context 'with records' do
+      before(:all) do
+        Log.new(url: '/', ips: Set.new(['0.0.0.0'])).save
+        Log.new(url: '/home', ips: Set.new(['0.0.0.0'])).save
+      end
+
+      after(:all) { Log.clear }
+
+      it 'returns humanize info' do
+        expect(described_class.index(colorized: false))
+        .to eq "/ was visited 1 times and there were 1 unique IPs\n" \
+             '/home was visited 1 times and there were 1 unique IPs'
+      end
+    end
+  end
+end
